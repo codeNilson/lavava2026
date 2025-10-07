4d6327d1e95b749f0ee886ade2e8f1e318c01d66
fcodenilson <fcodenilson@gmail.com>
2025-10-07 18:18:51 +0000

    fix import

--- src/main/kotlin/io/github/codenilson/lavava2026/players/PlayerRepository.kt
+++ src/main/kotlin/io/github/codenilson/lavava2026/players/PlayerRepository.kt
@@ -1,6 +1,6 @@
 package io.github.codenilson.lavava2026.players
 
-import org.springframework.data.jpa.JpaRepository
+import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface PlayerRepository: JpaRepository<Player, UUID> {

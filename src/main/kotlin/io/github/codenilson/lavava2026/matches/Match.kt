f86a8c02fd49708dc0f13dd1c5265c7aed0a5aca
fcodenilson <fcodenilson@gmail.com>
2025-10-08 16:03:42 +0000

    testing structure

--- src/main/kotlin/io/github/codenilson/lavava2026/matches/Match.kt
+++ src/main/kotlin/io/github/codenilson/lavava2026/matches/Match.kt
@@ -1,4 +1,4 @@
-package io.github.codenilson.lavava2026.players
+package io.github.codenilson.lavava2026.matches
 
 import io.github.codenilson.lavava2026.playersPerformances.PlayerPerformance
 import jakarta.persistence.Column
@@ -9,7 +9,6 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
-import jakarta.persistence.UniqueConstraint
 import java.time.ZonedDateTime
 import java.util.UUID
 import org.springframework.data.annotation.CreatedDate
@@ -17,23 +16,12 @@ import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 
 @Entity
+@Table(name = "matches")
 @EntityListeners(AuditingEntityListener::class)
-@Table(
-        name = "players",
-        uniqueConstraints = [UniqueConstraint(columnNames = ["game_name", "tag_name"])]
-)
-class Player(
+class Match(
         @Id @GeneratedValue(strategy = GenerationType.AUTO) var id: UUID,
-        @Column(nullable = false, unique = true) var puuid: String,
-        @Column(nullable = false) var gameName: String,
-        @Column(nullable = false) var tagName: String,
-
-        // TODO: Verify constraints after getting Riot API
-        var competitiveTier: Int? = null,
-        var playerCard: String? = null,
-        var playerTitle: String? = null,
-        var accountLevel: Int? = null,
-        @OneToMany(mappedBy = "player") var performances: List<PlayerPerformance> = mutableListOf(),
+        @Column(nullable = false, unique = true) var matchRiotId: String,
+        @OneToMany(mappedBy = "match") var performances: List<PlayerPerformance> = mutableListOf(),
         @LastModifiedDate @Column(nullable = false) var updatedAt: ZonedDateTime,
         @CreatedDate @Column(nullable = false, updatable = false) var createdAt: ZonedDateTime
 )
--- src/main/kotlin/io/github/codenilson/lavava2026/teams/Team.kt
+++ src/main/kotlin/io/github/codenilson/lavava2026/teams/Team.kt
@@ -1,27 +1,31 @@
 package io.github.codenilson.lavava2026.teams
 
+import io.github.codenilson.lavava2026.playersPerformances.PlayerPerformance
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
-import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.ZonedDateTime
 import java.util.UUID
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 
 @Entity
 @EntityListeners(AuditingEntityListener::class)
 @Table(name = "teams")
-class Team (
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    var id: UUID,
+class Team(
+        @Id @GeneratedValue(strategy = GenerationType.AUTO) var id: UUID,
 
-    // TODO: Maybe accept null and have a default value
-    var won: Boolean,
-
-    var roundsPlayed: Int,
-
-    var roundsWon: Int,
-
+        // TODO: Maybe accept null and have a default value
+        var won: Boolean,
+        var roundsPlayed: Int,
+        var roundsWon: Int,
+        @OneToMany(mappedBy = "team") var performances: List<PlayerPerformance> = mutableListOf(),
+        @LastModifiedDate @Column(nullable = false) var updatedAt: ZonedDateTime,
+        @CreatedDate @Column(nullable = false, updatable = false) var createdAt: ZonedDateTime
 )

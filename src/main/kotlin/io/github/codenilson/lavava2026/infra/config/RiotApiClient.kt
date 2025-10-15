665e9f162140509174d415d1d9445ad159fc818f
fcodenilson <fcodenilson@gmail.com>
2025-10-15 15:57:37 +0000

    fix

---
+++ src/main/kotlin/io/github/codenilson/lavava2026/infra/config/RiotApiClient.kt
@@ -0,0 +1,17 @@
+package io.github.codenilson.lavava2026.infra.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+
+@Configuration
+class RiotApiClient(@Value("\${api.base_url}") private val baseUrl: String) {
+    @Bean
+    fun webClient(builder: WebClient.Builder): WebClient {
+        return builder
+        .baseUrl(baseUrl)
+        .defaultHeader("Content-Type", "application/json")
+        .build()
+    }
+}

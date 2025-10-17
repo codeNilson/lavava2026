b752316b04e00bd2ddd0d0ee2966b5397b9d5812
fcodenilson <fcodenilson@gmail.com>
2025-10-17 18:57:29 +0000

    refactor: useMatchInfoDTO instead of ValorantMatchDTO on match mapper

--- src/main/kotlin/io/github/codenilson/lavava2026/application/mapper/MatchMapper.kt
+++ src/main/kotlin/io/github/codenilson/lavava2026/application/mapper/MatchMapper.kt
@@ -1,19 +1,19 @@
 package io.github.codenilson.lavava2026.application.mapper
 
 import io.github.codenilson.lavava2026.domain.matches.Match
-import io.github.codenilson.lavava2026.domain.valorant.dto.ValorantMatchDTO
+import io.github.codenilson.lavava2026.domain.valorant.dto.MatchInfoDto
 import org.springframework.stereotype.Component
 
 @Component
 class MatchMapper {
 
-    fun fromValorantMatch(valorantMatch: ValorantMatchDTO) : Match {
+    fun fromValorantMatch(valorantMatchInfo: MatchInfoDto) : Match {
         return Match(
-            matchRiotId = valorantMatch.matchInfo.matchId,
-            gameLength = valorantMatch.matchInfo.gameLengthMillis,
-            map = valorantMatch.matchInfo.mapId,
-            gameStartMillis = valorantMatch.matchInfo.gameStartMillis,
-            isCompleted = valorantMatch.matchInfo.isCompleted,
+            matchRiotId = valorantMatchInfo.matchId,
+            gameLength = valorantMatchInfo.gameLengthMillis,
+            map = valorantMatchInfo.mapId,
+            gameStartMillis = valorantMatchInfo.gameStartMillis,
+            isCompleted = valorantMatchInfo.isCompleted,
             season = "2026", // todo: verify how to get the season
         )
     }

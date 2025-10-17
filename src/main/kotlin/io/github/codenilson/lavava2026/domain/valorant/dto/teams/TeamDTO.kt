2bcd4af440558374c3a02378ca6b9c1a26784f87
codenilson <fcodenilson@gmail.com>
2025-10-17 19:26:33 -0300

    refactor: rename TeamDto to TeamDTO for consistency

--- src/main/kotlin/io/github/codenilson/lavava2026/domain/valorant/dto/teams/TeamDTO.kt
+++ src/main/kotlin/io/github/codenilson/lavava2026/domain/valorant/dto/teams/TeamDTO.kt
@@ -3,7 +3,7 @@ package io.github.codenilson.lavava2026.domain.valorant.dto.teams
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class TeamDto(
+data class TeamDTO(
     val teamId: String,
     val won: Boolean,
     val roundsPlayed: Int,

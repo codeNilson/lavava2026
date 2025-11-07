8bc230bd4aa5dbe0c63ed8f157ca2fe06a72a9e2
fcodenilson <fcodenilson@gmail.com>
2025-11-07 14:50:01 +0000

    add jackson anotattions

--- src/main/kotlin/io/github/codenilson/lavava2026/domain/valorant/dto/matches/ValorantMatchDTO.kt
+++ src/main/kotlin/io/github/codenilson/lavava2026/domain/valorant/dto/matches/ValorantMatchDTO.kt
@@ -1,30 +1,38 @@
 package io.github.codenilson.lavava2026.domain.valorant.dto.matches
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.github.codenilson.lavava2026.domain.valorant.dto.players.PlayerDTO
 import io.github.codenilson.lavava2026.domain.valorant.dto.rounds.RoundResultDTO
 import io.github.codenilson.lavava2026.domain.valorant.dto.teams.TeamDTO
+import java.util.UUID
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class ValorantMatchDTO(
-    val matchInfo: MatchInfoDTO,
+    @JsonProperty("metadata") val matchInfo: MatchInfoDTO,
     val players: List<PlayerDTO>,
     val teams: List<TeamDTO>,
-    val roundResults: List<RoundResultDTO>
-)
+    @JsonProperty("rounds") val roundResults: List<RoundResultDTO>
+) // TODO: kills on henrik
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class MatchInfoDTO(
     val matchId: String,
-    val mapId: String,
+    val map: Map,
     val gameVersion: String,
     val region: String,
-    val gameLengthMillis: Int,
+    @JsonProperty("game_length_in_ms") val gameLengthMillis: Long,
     val provisioningFlowId: String,
-    val gameStartMillis: Long,
+    @JsonProperty("started_at") val gameStartMillis: Long, // TODO: Date-time
+    // queue
     val isCompleted: Boolean,
     val customGameName: String,
     val gameMode: String,
     val isRanked: Boolean,
     val seasonId: String
+)
+
+data class Map(
+    val id: UUID,
+    val name: String,
 )

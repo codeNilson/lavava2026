3fef68aba570ed70c872f2e8a070cdca6418486f
fcodenilson <fcodenilson@gmail.com>
2025-11-11 19:46:40 +0000

    chore: initial config

--- src/main/kotlin/io/github/codenilson/lavava2026/application/services/MatchService.kt
+++ src/main/kotlin/io/github/codenilson/lavava2026/application/services/MatchService.kt
@@ -1,6 +1,7 @@
 package io.github.codenilson.lavava2026.application.services
 
 import io.github.codenilson.lavava2026.application.exceptions.ResourceAlreadyExists
+import io.github.codenilson.lavava2026.application.usecases.SyncMatchUseCase
 import io.github.codenilson.lavava2026.domain.matches.Match
 import io.github.codenilson.lavava2026.domain.matches.MatchRepository
 import io.github.codenilson.lavava2026.domain.valorant.dto.matches.ValorantMatchDTO
@@ -11,6 +12,7 @@ import reactor.core.publisher.Mono
 class MatchService(
     private val matchRepository: MatchRepository,
     private val valorantIntegrationService: ValorantIntegrationService,
+    private val syncMatchUseCase: SyncMatchUseCase,
 //    private val matchMapper: MatchMapper,
 ) {
 //    fun saveFromValorantMatch(valorantMatch: ValorantMatchDTO): Match {
@@ -23,11 +25,21 @@ class MatchService(
 //        return matchRepository.save(match)
 //    }
 
+    fun syncMatch(matchId: String) {
+        if (matchAlreadyExists(matchId)) {
+            throw ResourceAlreadyExists("Match with id $matchId already exists")
+        }
+
+        val valorantMatch = valorantIntegrationService.fetchMatch(matchId).block() ?: throw Exception("Match not found")
+
+        syncMatchUseCase.sync(valorantMatch)
+    }
+
     fun matchAlreadyExists(matchId: String): Boolean {
         return matchRepository.existsByMatchRiotId(matchId)
     }
 
-    // teste
+    // função temporária só para testes.
     fun getValorantMatchInfo(matchId: String): ValorantMatchDTO {
         return valorantIntegrationService.fetchMatch(matchId).block() ?: throw Exception("Match not found")
     }
